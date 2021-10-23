815b8c079f215f892a5bdcb7d08b73a77d4c947c
Cesar Augusto Tapia <cesart050892@users.noreply.github.com>
2021-10-22 21:42:25 -0600

    agrgando direccion de fpdf

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -64,7 +64,9 @@ class Autoload extends AutoloadConfig
      *
      * @var array<string, string>
      */
-    public $classmap = [];
+    public $classmap = [
+        'FPDF' => APPPATH . 'ThirdParty/fpdf/fpdf.php',
+    ];
 
     /**
      * -------------------------------------------------------------------
